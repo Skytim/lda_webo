b2cb42e1e33fcf0627186b2625cc4640ba5a1767
Sky Tim <sky23357168@@gmail.com>
2016-02-15 17:23:51 +0800

    second commit

--- lda_test20160201.py
+++ lda_test20160201.py
@@ -17,32 +17,38 @@ jieba.analyse.set_stop_words("jieba/stop_words.txt")
 # 儲存斷詞陣列
 
 notwants=[]
-f = open('data/test2.csv', 'r')
+f = open('data/testcsv.csv', 'r')
 not_want = open('jieba/not_want.txt', 'r')
 for row in csv.reader(not_want):
     notwants.append(', '.join(row).decode('utf-8'))
-
+finalcontent=[]
 words=[]
 for row in csv.reader(f):
     content=', '.join(row).decode('utf-8')
     content=content.replace(" ","")
 
     if(content.find('//@u')==-1 and content.find(u'转发微博')==-1 and content.find(u'转发微博')==-1):
-        if not any(word in content for word in notwants):
-            # jieba_word=jieba.analyse.extract_tags(content,20)
-            psegwords = pseg.cut(content)
+        if not any(word in content for word in notwants ):
+            if(len(content)>20):
+                # jieba_word=jieba.analyse.extract_tags(content,20)
+                psegwords = pseg.cut(content)
 
-            # for line in jieba_word:
-            #     print line.encode('utf-8')
-            # 從此開始
-            nounword=[]
-            for word in psegwords:
-                if(word.flag=="n" and word.word!="" and word.word!= " "):
-                    nounword.append(word.word)
-            # print nounword
-            # print "Hello world"
-            # print list(jieba_word)
-            words.append(nounword)
+                # for line in jieba_word:
+                #     print line.encode('utf-8')
+                # 從此開始
+                nounword=[]
+
+                for word in psegwords:
+                    if(word.flag=="n" and word.word!="" and word.word!= " "):
+                        nounword.append(word.word)
+                   
+                # print "Hello world"
+                # print list(jieba_word)
+            
+                if (nounword!=[]):
+                    finalcontent.append(content)
+                    words.append(nounword)
+                # print nounword
 print "文字讀取結束"
 dic = corpora.Dictionary(words)
 print "轉換為字典"
@@ -53,14 +59,32 @@ tfidf = models.TfidfModel(corpus)
 corpus_tfidf = tfidf[corpus]#此处已经计算得出所有评论的tf-idf 值
 # print tfidf[vec]
 corpus_tfidf = tfidf[corpus]
+documentsort=[]
 if(words!=[[]]):
     # lsi = models.LsiModel(corpus_tfidf, id2word=dic, num_topics=10)
     lda = gensim.models.ldamodel.LdaModel(corpus_tfidf, id2word=dic, num_topics=20, update_every=1, chunksize=10000, passes=1)
     lda_Out=lda.print_topics(20)
 
-    for line in lda_Out:
-        for word in line:
-            print word
+    # for line in lda_Out:
+    #     for word in line:
+    #         print word
+    corpus_lda = lda[corpus_tfidf]
+    # print corpus_lda[0]
+    # print sorted(corpus_lda[0], key=lambda x: x[1])[-1]
+   
+    # print sorted(corpus_lda[0], key=lambda x: x[1])[-1][0]
+    
+    
+    for doc in corpus_lda:       
+        documentsort.append(sorted(doc, key=lambda x: x[1])[-1][0])
+
+
+
+for x in range(len(documentsort)):
+    print str(x)+" ,"+str(documentsort[x])+" ,"+finalcontent[x]
+
+print len(documentsort)
+print len(finalcontent)
 query = u"女孩"
 
 query_bow = dic.doc2bow(list(jieba.cut(query)))
